ce3fd822425130a3de5979f8159bd0b456409f34
boberovsky <boberp26@gmail.com>
2019-07-05 23:03:46 +0200

    initial commit

---
+++ src/main/java/pl/patrykbober/spaceflights/SpaceflightsApplication.java
@@ -0,0 +1,15 @@
+package pl.patrykbober.spaceflights;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceflightsApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(SpaceflightsApplication.class, args);
+	}
+
+}
